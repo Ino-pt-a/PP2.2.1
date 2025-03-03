540da871cc82a188159dc746a371ec980341acc0
Dina Riella <riella.dandj@gmail.com>
2025-03-03 20:00:53 +0300

    Update UserDaoImp.java
    join fetch

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -24,14 +24,15 @@ public class UserDaoImp implements UserDao {
    @Override
    @SuppressWarnings("unchecked")
    public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      String hql = "SELECT DISTINCT u FROM User u JOIN FETCH u.car";
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery(hql, User.class);
       return query.getResultList();
    }
 
 
    @Override
    public User getUserByCarModelAndSeries(String model, int series) {
-      String hql = "FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+      String hql = "FROM User u JOIN FETCH u.car c WHERE c.model = :model AND c.series = :series";
       TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql, User.class);
       query.setParameter("model", model);
       query.setParameter("series", series);
